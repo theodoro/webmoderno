13ab994c2e4eec0c70dfd8bc8ee4b94a2d181401
Bruno Theodoro <theodorodeaquino@gmail.com>
2020-08-14 07:25:44 -0300

    final da aula introdutoria de variaveis

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 // Comentário em JavaScript 
+// Evite comentários obvios prefira um código limpo
 
+//variaveis e constantes
+
+var a = 3 //os nomes das var são muitos importantes, sempre coloque bons nomes
+let b = 4 // let também cria variaveis, mas é uma forma mais moderna
+var a = 20
+ b = 30
+
+console.log(a,b)
+
+a = 300
+b = 2000
+
+console.log(a,b)
+
+const c = 5
+console.log(c)
