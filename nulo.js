f892fa421bd30679075f439936034db12041a150
Bruno Theodoro <theodorodeaquino@gmail.com>
2020-08-23 10:53:37 -0300

    Aulas de nulo e underfined

---
+++ nulo.js
@@ -0,0 +1,9 @@
+const a = {name: 'teste'}
+undefined
+console.log(a)
+
+let c = 3 // Quando faço uma atribução por tipo primitivos eu paço o valor, no caso de obj é referencia
+let d = c 
+d++ 
+console.log(d)
+console.log(c)
