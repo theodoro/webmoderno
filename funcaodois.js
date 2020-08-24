23c2fcd9e789b99c504a5b90225d16bbe0b32aec
Bruno Theodoro <theodorodeaquino@gmail.com>
2020-08-24 07:23:42 -0300

    estudos de funções dois

---
+++ funcaodois.js
@@ -0,0 +1,17 @@
+//armazenando uma função dentro de uma váriavel 
+const imprimeSoma = function(a, b){
+
+    console.log(2+3)
+}
+imprimeSoma(2,3)
+
+//Armazenando um função arrow em uma váriavel
+
+const soma = (a,b) => {
+    return a + b
+}
+
+console.log(soma(3,5))
+
+const subtracao = (a,b) => a - b
+console.log(subtracao(4,1))
