90b9e7912c6bb714824305fa590b34fea2ab0da0
Bruno Theodoro <theodorodeaquino@gmail.com>
2020-09-01 07:21:10 -0300

    Operadores relacionais

---
+++ aritimeticos.js
@@ -0,0 +1,8 @@
+const [a, b, c, d] = [3, 4, 5, 6]
+const soma = a + b + c + d
+const sub = d - b
+const multiplicacao = a * b
+const divisao = d / a 
+const modulo = a % 2
+
+console.log(soma, sub, multiplicacao, divisao, modulo)
