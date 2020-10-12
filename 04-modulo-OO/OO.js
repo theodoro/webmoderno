01eb1b47f15382fcd9129026531a6d7445d7def9
Bruno Theodoro <theodorodeaquino@gmail.com>
2020-10-12 09:33:12 -0300

    teoria objetos

---
+++ 04-modulo-OO/OO.js
@@ -0,0 +1,22 @@
+//Código não executável
+
+//Procedural 
+processamento(valor1, valor2, valor3)
+
+// OO
+
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+    processamento(){
+        // ...
+    }
+}
+
+objeto.processamento() //Foco passou a ser objeto
+
+//Abstração 
+//Encapsulamento 
+//Herança
+//POLIFORMISMO
