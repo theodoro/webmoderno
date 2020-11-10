fed7e52af70f3cf52bd7a0b3fb6014f8008dfee7
Bruno Theodoro <theodorodeaquino@gmail.com>
2020-11-10 07:02:59 -0300

    Herança e Array js

---
+++ 05-Array/array.js
@@ -0,0 +1,23 @@
+let aprovados = new Array ('Bia', 'Carlos', 'Ana')
+console.log(aprovados)
+
+aprovados = ['Bia', 'Carlos', 'Ana']
+console.log(aprovados[0])
+console.log(aprovados[1])
+console.log(aprovados[2])
+console.log(aprovados[3])
+
+aprovados[3] = 'Paulo'
+aprovados.push('Sumara')
+console.log(aprovados.length)
+
+aprovados[9] = 'Rafael'
+console.log(aprovados.length)
+
+console.log(aprovados)
+aprovados.sort
+console.log(aprovados)
+
+aprovados = ['Bia', 'Carlos', 'Ana']
+aprovados.splice(1,2,'Elemento1', 'Elemento2')
+console.log(aprovados)
