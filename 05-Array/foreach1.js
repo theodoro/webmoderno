282364b24318454932a16237f3dd8f6cc95b3f45
Bruno Theodoro <theodorodeaquino@gmail.com>
2020-11-16 06:48:31 -0300

    Array percorrendo com For

---
+++ 05-Array/foreach1.js
@@ -0,0 +1,10 @@
+const aprovados = ['Bruno', 'Sumara', 'Gustavo', 'Larissa']
+
+aprovados.forEach(function(nome, indice){
+    console.log(`${indice + 1}) ${nome} `)
+})
+
+aprovados.forEach(nome => console.log(nome))
+
+const exibirAprovados = aprovados => console.log(aprovados)
+aprovados.forEach(exibirAprovados)
