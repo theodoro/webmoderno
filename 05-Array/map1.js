140dae624f28be72244ed61f128e8e14be2c8248
Bruno Theodoro <theodorodeaquino@gmail.com>
2020-11-17 06:34:05 -0300

    Função MAP

---
+++ 05-Array/map1.js
@@ -0,0 +1,17 @@
+const num = [1,2,3,4,5]
+
+// For com proposito
+
+let resultado = num.map(function(e){
+    return e * 2
+})
+
+console.log(resultado)
+
+const soma10 = e => e + 10
+const triplo = e => e * 3
+
+const paraDinheiro = e => `R$ ${parseFloat(e).toFixed(2).replace('.', ',')}`
+
+resultado = num.map(soma10).map(triplo).map(paraDinheiro)
+console.log(resultado)
