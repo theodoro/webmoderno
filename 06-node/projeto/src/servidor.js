92073ad8576d19dcf5d1199d18d48684ce662d60
Bruno Aquino <theodorodeaquino@gmail.com>
2021-05-09 07:16:06 -0300

    Modificando o arquivo servidor e criando banco de dados

--- 06-node/projeto/src/servidor.js
+++ 06-node/projeto/src/servidor.js
@@ -2,11 +2,24 @@ const porta = 3003 //Uma porta
 
 const express = require('express')
 const app = express()
+const bancoDeDados = require('./bancodedados')
 
 
 app.get('/produtos', (req, res, next) => {
-    res.send({nome: 'Notebook', preco: 123.45}) // converter para JSON
+    res.send(bancoDeDados.getProdutos())
 }) 
+
+app.get('/produtos/:id', (req, res, next) =>{
+    res.send(bancoDeDados.getProduto(req.params.id))
+})
+
+app.post('/produtos', (req, res, next) =>{
+    const produto = bancoDeDados.salvarProduto({
+        nome: req.body.name,
+        preco: req.body.preco
+    })
+    res.send(produto) //JSON
+})
 
 app.listen(porta, () =>{
     console.log(`Servidor executando na porta ${porta}.`)
