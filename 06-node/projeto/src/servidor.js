cc55914b2fa7ae4453c344955f6cbe1f43a6476f
Bruno Aquino <theodorodeaquino@gmail.com>
2021-05-13 21:15:45 -0300

    CRUD

--- 06-node/projeto/src/servidor.js
+++ 06-node/projeto/src/servidor.js
@@ -2,8 +2,10 @@ const porta = 3003 //Uma porta
 
 const express = require('express')
 const app = express()
+//const bodyParset = require('body-parset')
 const bancoDeDados = require('./bancodedados')
 
+app.use(bodyParset.urlencoded({ extends: true}))
 
 app.get('/produtos', (req, res, next) => {
     res.send(bancoDeDados.getProdutos())
@@ -19,6 +21,20 @@ app.post('/produtos', (req, res, next) =>{
         preco: req.body.preco
     })
     res.send(produto) //JSON
+})
+
+app.put('/produtos/:id', (req, res, next) =>{
+    const produto = bancoDeDados.salvarProduto({
+        id: req.params.id,
+        nome: req.body.name,
+        preco: req.body.preco
+    })
+    res.send(produto) //JSON
+})
+
+app.delete('/produtos/:id', (req, res, next) => {
+    const produto = bancoDeDados.excluirProduto(req.params.id)
+    res.send(produto)
 })
 
 app.listen(porta, () =>{
